807e65faef0cc81600d2fb9d78e18cd003f596fc
Ahamed Maahin. <ahamaasidrah@gmail.com>
2025-09-26 21:06:57 -0600

    Added Jump:
    Currently working on the bug the code has and will be sorting it out soon and commit it on the next attempt and push it.

--- Assets/Project_OnevsOnes/Scripts/Player/PlayerController.cs
+++ Assets/Project_OnevsOnes/Scripts/Player/PlayerController.cs
@@ -1,9 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] public float playerMoveSpeed;
+    [SerializeField] public float playerJumpForce;
     private Rigidbody2D _rigidbody2D;
+    private bool _isGrounded;
+    
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private float _groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask _groundLayer;
     
     void Awake()
     {
@@ -18,5 +25,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         _rigidbody2D.linearVelocity = new Vector2(playerMoveSpeed * Time.deltaTime, _rigidbody2D.linearVelocity.y);
+        
+       //Jump Input:
+       if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) && _isGrounded)
+       {
+           _rigidbody2D.linearVelocity = Vector2.up * playerJumpForce * Time.deltaTime;
+       }
+    }
+
+    void FixedUpdate()
+    {
+        _isGrounded = Physics2D.OverlapCircle (_groundCheck.position, _groundCheckRadius, _groundLayer);
     }
 }
