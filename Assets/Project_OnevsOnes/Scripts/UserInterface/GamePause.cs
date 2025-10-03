e165d3a8e2f0532c08378b0eaca4cfb6cf3aa2d1
Ahamed Maahin. <ahamaasidrah@gmail.com>
2025-10-02 21:58:44 -0600

    Game Pause Implementation:
    GamePause implementation has been done and works fine as needed while testing.

---
+++ Assets/Project_OnevsOnes/Scripts/UserInterface/GamePause.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePause : MonoBehaviour
+{
+    
+    [SerializeField] public Canvas gamePauseCanvas;
+    public bool isPaused = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gamePauseCanvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        gamePauseCanvas.enabled = true;
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        gamePauseCanvas.enabled = false;
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("UI_MainMenu");
+    }
+    
+}
