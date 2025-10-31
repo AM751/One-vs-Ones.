bb0036241174ac36eef24674cc1f602e648f30d5
Ahamed Maahin. <ahamaasidrah@gmail.com>
2025-10-31 09:35:33 -0600

    Added Instructions:
    Added the instructions for the player's convenience. Reworked on the obstacles too by changing its scaling and colour.

--- Assets/Project_OnevsOnes/Scripts/UserInterface/MainMenu.cs
+++ Assets/Project_OnevsOnes/Scripts/UserInterface/MainMenu.cs
@@ -3,11 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Canvas References")]
+    [SerializeField] private Canvas _instructionCanvas;
     public void StartGame()
     {
         SceneManager.LoadScene("GPS_Lvl1");
     }
 
+    public void goToInstructions()
+    {
+        _instructionCanvas.enabled = true;
+    }
+
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene("UI_MainMenu");
+    }
     public void QuitGame()
     {
         Application.Quit();
