9437887b964c4356cf14e5dd5195ce8ee67c9e1a
Ahamed Maahin. <ahamaasidrah@gmail.com>
2025-11-09 15:27:07 -0700

    Player Knockback System Feedback:
    Now if the player collides with the obstacle, they'll instantly get an update on how to regain their pace to progress further into the level.

--- Assets/Project_OnevsOnes/Scripts/Obstacle/Obstacle.cs
+++ Assets/Project_OnevsOnes/Scripts/Obstacle/Obstacle.cs
@@ -1,12 +1,38 @@
+using System;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    [Header("Player Particle Effect.")] [SerializeField]
+    [Header("Player Object.")] [SerializeField]
     private GameObject _player;
     
     [Header("Player Particle Effect.")]
     [SerializeField] private ParticleSystem _playerCollidedParticles; 
+    
+    [Header("Player Knockback Instruction.")]
+    [SerializeField] private Canvas _playerKnockbackInstructionCanvas;
+    private bool _isPlayerKnockedBackInstructionActive = false;
+
+
+    private void Start()
+    {
+        if (_playerKnockbackInstructionCanvas != null)
+        {
+            _playerKnockbackInstructionCanvas.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (_isPlayerKnockedBackInstructionActive)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                KnockBackInstructionDisable();
+            }
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject == _player)
@@ -14,6 +40,25 @@ public class Obstacle : MonoBehaviour
             Vector2 collidingObstacle = other.GetContact(0).point;
             Instantiate(_playerCollidedParticles, collidingObstacle, Quaternion.identity);
             PlayerAudio.Instance.playSoundOnObjectCollide();
+            KnockBackInstructionEnable();
+        }
+    }
+
+    private void KnockBackInstructionEnable()
+    {
+        if (_playerKnockbackInstructionCanvas != null)
+        {
+            _playerKnockbackInstructionCanvas.enabled = true;
+            _isPlayerKnockedBackInstructionActive = true;
+        }
+    }
+
+    private void KnockBackInstructionDisable()
+    {
+        if (_playerKnockbackInstructionCanvas != null)
+        {
+            _playerKnockbackInstructionCanvas.enabled = false;
+            _isPlayerKnockedBackInstructionActive = false;
         }
     }
 }
