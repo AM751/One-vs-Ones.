2960f685c10f6fb9b59eb3a35029e515850ea277
Ahamed Maahin. <ahamaasidrah@gmail.com>
2025-12-14 00:27:28 +0530

    Portal message:
    Final portal message added and with this all the tutorials are done and Runner phase has to be added soon.

--- Assets/Project_OnevsOnes/Scripts/UserInterface/LevelBeaten.cs
+++ Assets/Project_OnevsOnes/Scripts/UserInterface/LevelBeaten.cs
@@ -10,7 +10,7 @@ public class LevelBeaten : MonoBehaviour
     {
         _levelBeatenCanvas.enabled = false;
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == _player)
         {
